6aa90519d8c8b2ad528aec7f6c0aff1c7604f586
Shubham Kumar <130787773+student12as@users.noreply.github.com>
2023-09-30 12:16:39 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
     START_TXT = """<b>Hello {},
-Mʏ Nᴀᴍᴇ Is <a href=https://t.me/{}>{}</a>,\n\nTrust me ! I can't even imagine how super-fast i can drive your Database channel 😍<b>"""
+Mʏ Nᴀᴍᴇ Is <a href=https://t.me/{}>{}</a>, Trust me ! I can't even imagine how super-fast i can drive your Database channel 😍<b>"""
     LZTHMB_TEXT = """Hello {},
 Glad to see you here. It seems that you really love <a href=https://t.me/LazyDeveloperr >LazyDeveloper's</a> work.\n\n<b>Thumbnail extracting</b> feature will be available soon, please join <a href=https://t.me/LazyDeveloper>Dev Channel</a> and stay tuned for next <a href=https://t.me/LazyDeveloper>update</a>.\n\n  🐞 Report Bug here: <a href=http://t.me/LazyDeveloperSupport>LazyDev Support</a>
     """
